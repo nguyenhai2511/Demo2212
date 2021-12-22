d484a8d4a7c41039802adad4dc8acea4d39ff67a
gitName <a@b.c>
2021-12-22 10:19:51 +0700

    first commit

---
+++ src/main/java/com/example/demo2212/Demo2212Application.java
@@ -0,0 +1,13 @@
+package com.example.demo2212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Demo2212Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2212Application.class, args);
+    }
+
+}
